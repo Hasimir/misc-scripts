34836473f35e322035bb3000ac1660a9305b15a0
Ben McGinnes <ben@adversary.org>
2015-01-08 13:39:09 +1100

    Rough beginning.

---
+++ python3/btcotc-events/README.rst
@@ -0,0 +1,10 @@
+=============================
+Bitcoin-OTC Events DB Scripts
+=============================
+
+May get moved to their own db when I can be bothered or have enough of
+them to justify it.  This is rough, proof-of-concept work.
+
+If you really want or need a license, use my standard BSD license
+(picked because I've got a generator for that).
+
---
+++ python3/btcotc-events/eventcheck.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import requests
+
+etype = input("Enter the type of check (e.g. nick, host, etc.): ")
+target = input("Enter the subject of search (e.g. handle, hostname, etc.): ")
+
+dom = "http://www.oedb.info"
+
+url = "{0}/{1}/{2}".format(dom, etype, target)
+
+payload = { "format": "json" }
+
+r = requests.get(url, params=payload)
+
+eventsnum = len(r.json()["events"])
+
+if eventsnum == 0:
+    print("There have been no events for the {0} {1}.".format(etype, target))
+else:
+    print("There have been {0} events for the {1} {2}.".format(
+        eventsnum, etype, target))
