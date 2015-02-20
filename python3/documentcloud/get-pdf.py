53c8e3b06af59f45d2966342b8e34561edba42b2
Ben McGinnes <ben@adversary.org>
2015-02-20 13:07:49 +1100

    Added copyright and usage info.

--- python3/documentcloud/get-pdf.py
+++ python3/documentcloud/get-pdf.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 
+##
+# Copyright (C) Ben McGinnes, 2013-2015
+# ben@adversary.org
+# OpenPGP/GPG key:  0x321E4E2373590E5D
+#
+# Version:  0.0.2
+#
+# BTC:  19J2Dc5DTztWRWMPcLwshj96NAnXAxtCvk
+# License:  3-clause BSD
+#
+# Requirements:
+#
+# * Python 3.2 or later (developed with Python 3.4.x)
+#
+# Options and notes:
+#
+# Usage:  Just run it and respond to the prompts.
+#
+# The document ID can usually be found in the HTML source to a web
+# page, usually encased in some javascript to make those annoying
+# viewers which "prevent" downloading the file.
+#
+##
+
 import requests
 
 doc_id = input("Enter the document ID (e.g. 12345-foo-bar): ")
