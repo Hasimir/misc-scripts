8653cf6293b99a431beaea85453842690691459f
Ben McGinnes <ben@adversary.org>
2015-07-12 15:30:04 +1000

    new script
    * Converts a printed GPG fingerprint with spaces to a copy & paste
      format for key IDs.

---
+++ python3/gpg-bits/fpr2key.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import sys
+
+if len(sys.argv) < 2:
+    fpr = input("Enter the fingerprint: ")
+else:
+    fpr = sys.argv[1:]
+
+fkid = "".join(fpr.split())
+lkid = fkid[-16:]
+skid = fkid[-8:]
+
+info = """
+The key full key ID (fingerprint) is:
+
+    {0}
+    {1}
+
+ The long key ID is:  0x{2}
+The short key ID is:  0x{3}
+""".format(fpr, fkid, lkid, skid)
+
+print(info)
