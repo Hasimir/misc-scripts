bb0ca1d0a897cdc983f440fb007a0af5b154777c
Ben McGinnes <ben@adversary.org>
2015-07-26 01:24:10 +1000

    Updated to widen scope:
    	* Can now enter one filename or one directory.
    	* If a directory, it will attempt to modify all files therein.
    	* will not touch sym links.
    	* Will handle both byte and string literals.

--- python3/linereplace.py
+++ python3/linereplace.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+from __future__ import unicode_literals
+
 ##
-# Copyright (C) Ben McGinnes, 2013-2014
+# Copyright (C) Ben McGinnes, 2013-2015
+# Copyright © Ben McGinnes, 2013-2015
 # ben@adversary.org
 # OpenPGP/GPG key:  0x321E4E2373590E5D
 #
-# Version:  0.0.1
+# Version:  0.0.2
 #
 # BTC:  19J2Dc5DTztWRWMPcLwshj96NAnXAxtCvk
 # License:  GPLv3
@@ -15,6 +18,7 @@
 # Requirements:
 #
 # * Python 3.2 or later (developed with Python 3.4.x)
+# * Might work with more recent versions of 2.7.x.
 #
 # Options and notes:
 #
@@ -23,41 +27,87 @@
 ##
 
 __author__ = "Ben McGinnes <ben@adversary.org>"
-__copyright__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
-__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
+__copyright__ = "Copyright © Benjamin D. McGinnes, 2013-2015"
 __license__ = "GPLv3"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __bitcoin__ = "19J2Dc5DTztWRWMPcLwshj96NAnXAxtCvk"
 
+import os
+import os.path
 import re
 import sys
 
-l = len(sys.argv)
+ls = os.listdir
+op = os.path
+sa = sys.argv
+l = len(sa)
+modded = []
 
 if l == 2:
-    filename = sys.argv[1]
+    folder = sa[1]
     pattern = input("Enter the pattern to match and remove: ")
     logrus = input("Enter the pattern to replace with: ")
 elif l == 3:
-    filename = sys.argv[1]
-    pattern = sys.argv[2]
+    folder = sa[1]
+    pattern = sa[2]
     logrus = input("Enter the pattern to replace with: ")
 elif l >= 4:
-    filename = sys.argv[1]
-    pattern = sys.argv[2]
-    logrus = sys.argv[3]
+    folder = sa[1]
+    pattern = sa[2]
+    logrus = sa[3]
 else:
-    filename = input("Enter the name of the file: ")
+    folder = input("Enter the path to the file(s): ")
     pattern = input("Enter the pattern to match and remove: ")
     logrus = input("Enter the pattern to replace with: ")
 
-f = open(filename, "r")
-lines = f.readlines()
-f.close()
+if op.isdir(folder) is False:
+    if op.isfile(folder) is True:
+        files = []
+        files.append(folder)
+    elif op.isfile is False and len(folder) == 0:
+        files = ls(op.curdir)
+    else:
+        files = folder
+elif op.isdir(folder) is True:
+    files = ls(folder)
+else:
+    files = None
+    print("You must enter a valid directory or a filename.")
+    pass
 
-f = open(filename, "w")
-for line in lines:
-    if pattern in line:
-        line = re.sub(pattern, logrus, line)
-    f.write(line)
-f.close()
+if files is not None:
+    for filename in files:
+        if op.isfile(filename) is True and op.islink(filename) is False:
+            modded.append(filename)
+            try:
+                f = open(filename, "r")
+                lines = f.readlines()
+                f.close()
+
+                f = open(filename, "w")
+                for line in lines:
+                    if pattern in line:
+                        line = re.sub(pattern, logrus, line)
+                    f.write(line)
+                f.close()
+            except:
+                f = open(filename, "rb")
+                lines = f.readlines()
+                f.close()
+
+                f = open(filename, "wb")
+                for line in lines:
+                    if pattern.encode("utf-8") in line:
+                        line = re.sub(pattern.encode("utf-8"),
+                                      logrus.encode("utf-8"), line)
+                    f.write(line)
+                f.close()
+else:
+    print("There are no files to process.")
+    pass
+
+if len(modded) > 0:
+    print("{0} files were able to be modified.".format(len(modded)))
+else:
+    print("No files were able to be modified.")
+    pass
