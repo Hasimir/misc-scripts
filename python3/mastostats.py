dfc756f320bd4927a70b6f44453ed86cf1d13786
Ben McGinnes <ben@adversary.org>
2018-05-04 03:48:44 +1000

    P[leroma] equals NP[leroma]
    * Prevented error (index error exception) when a Pleroma or other
      non-Mastodon instance is checked along with Mastodon instances.
    * Does not actually provide real stats for Pleroma instances, but
      won't fail hard now.

--- python3/mastostats.py
+++ python3/mastostats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-from future import unicode_literals
+# -*- coding: utf-8 -*-
 
 import requests
 import sys
@@ -10,19 +9,24 @@ if len(sys.argv) >= 2:
 else:
     urls = input("Enter the URL(s) of the instances to check: ")
 
+pleromas = []
+
 for i in range(len(urls)):
     strings = []
     integers = []
     floats = []
+    
     if urls[i].startswith("https://") is True:
         url = urls[i]
     elif urls[i].startswith("http://") is True:
         url = urls[i].replace("http://", "https://")
     else:
         url = "https://{0}".format(urls[i])
+    
     weakurl = url.replace("https://", "http://")
     path = "about/more"
     host = url.replace("https://", "")
+
     try:
         uri = "{0}/{1}".format(url, path)
         r = requests.get(uri, verify=True)
@@ -31,16 +35,36 @@ for i in range(len(urls)):
         uri = "{0}/{1}".format(weakurl, path)
         r = requests.get(uri)
         r_url = weakurl
-    lines = r.content.decode().splitlines()
-    for line in lines:
-        if line.startswith("<strong>") is True:
-            newline = line.replace("<strong>", "").replace("</strong>", "")
-            strings.append(newline)
-            numline = newline.replace(",", "")
-            integers.append(int(numline))
-            floats.append(float(numline))
-        else:
-            pass
+
+    if r.content.decode().lower().find("pleroma") >= 1:
+        pleromas.append(host)
+    else:
+        pass
+
+    if len(pleromas) >= 1:
+        for n in range(len(pleromas)):
+            if r.url.count(pleromas[n]) == 1:
+                strings.append("unknown")
+                strings.append("unknown")
+                strings.append("an unknown number of")
+            else:
+                lines = r.content.decode().splitlines()
+                for line in lines:
+                    if line.startswith("<strong>") is True:
+                        newline = line.replace("<strong>", "").replace("</strong>", "")
+                        strings.append(newline)
+                        numline = newline.replace(",", "")
+                        integers.append(int(numline))
+                        floats.append(float(numline))
+    else:
+        lines = r.content.decode().splitlines()
+        for line in lines:
+            if line.startswith("<strong>") is True:
+                newline = line.replace("<strong>", "").replace("</strong>", "")
+                strings.append(newline)
+                numline = newline.replace(",", "")
+                integers.append(int(numline))
+                floats.append(float(numline))
 
     result = """
   Current {0} stats are:
@@ -52,3 +76,11 @@ for i in range(len(urls)):
 """.format(host, strings[0], strings[1], r_url, strings[2])
 
     print(result)
+
+if len(pleromas) >= 1:
+    print("")
+    for n in range(len(pleromas)):
+        print("{0} is a Pleroma instance.".format(pleromas[n]))
+    print("")
+else:
+    pass
